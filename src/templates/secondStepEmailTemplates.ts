2ecbbc17a6de11dde0865574cc4079991253a895
Rifqy Zacky Ariadhy <rifqyzackya@gmail.com>
2023-02-11 11:12:37 +0700

    console.log

--- src/templates/secondStepEmailTemplates.ts
+++ src/templates/secondStepEmailTemplates.ts
@@ -28,7 +28,8 @@ type candidateKey =
 
 const registrationSecondStepEmail = function (dataString: string) {
   const data: ISecondStepEmailData = JSON.parse(dataString);
-  const photo = path.join(process.cwd(), data.candidate.photo);
+    const photo = path.join(process.cwd(), data.candidate.photo);
+    console.log(photo, '============================== photo')
   const photoUrl = "data:image/png;base64," + fs.readFileSync(photo, { encoding: "base64" });
   
   const html = `
