4da7afe4208430fecb15926451bf9d40a85379a5
Rifqy Zacky Ariadhy <rifqyzackya@gmail.com>
2023-02-11 11:23:02 +0700

    update

--- src/controllers/mail.ts
+++ src/controllers/mail.ts
@@ -105,7 +105,7 @@ export const generateDataForSecondStepEmail = (classId: number, cityId: number):
                     where: {
                         registrationId: item.id,
                         deletedAt: {
-                            [Op.ne]: null,
+                            [Op.eq]: null,
                         },
                     },
                 });
